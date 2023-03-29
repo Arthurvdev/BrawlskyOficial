8dbfebae79f178bf19a5aba7c09965ffe9f88d2a
Arthurvdev <99282591+Arthurvdev@users.noreply.github.com>
2023-03-29 14:01:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+//Script navbar
+
+const searchInput = document.querySelector('#search-input');
+const products = document.querySelectorAll('.product');
+
+function search() {
+  const term = searchInput.value.trim().toLowerCase();
+  if (term) {
+    products.forEach(product => {
+      const name = product.querySelector('h1').textContent.toLowerCase();
+      if (name.includes(term)) {
+        product.style.display = 'block';
+      } else {
+        product.style.display = 'none';
+      }
+    });
+  } else {
+    products.forEach(product => {
+      product.style.display = 'block';
+    });
+  }
+}
+
+searchInput.addEventListener('keyup', search);
+
+//
+
+
+
+
